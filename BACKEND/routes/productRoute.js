23f10a76fe281b5d6ba9d58f9783cc0be33bd2d9
HarshanaWAJ <janithharshana2019@gmail.com>
2025-07-15 17:47:32 +0530

    Cart Cruds Completed

--- BACKEND/routes/productRoute.js
+++ BACKEND/routes/productRoute.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
+const auth = require('../middlewares/authMiddleware');
+
 // Importing the product controller
 const {                       
     createProduct,
